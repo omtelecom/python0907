9abf8ad135e91662e3176e1a894bbd787172dc51
Roman Golovko <Roman Golovko>
2016-07-15 21:38:58 +0300

    Home DZ

--- modul01/dz2.py
+++ modul01/dz2.py
@@ -1,2 +1,9 @@
+def square_even(n):
+    for i in range(2, n + 1 ):
+        if i % 2 == 0:
+            print(i ** 2)
+    return i
 
+
+print (square_even(7))
 
--- modul01/dz3.py
+++ modul01/dz3.py
@@ -1,5 +1,1 @@
-def square_even(n):
-    list = [i * i for i in range(2, n + 1 ) if i % 2 == 0]
-    return list
 
-print (square_even(6))
